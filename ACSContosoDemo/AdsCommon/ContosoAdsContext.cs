a8882797dec2c7ddd1b22430643bdc88b1f14011
Lucky Behera <vickybehera332@gmail.com>
2021-04-07 23:33:26 +0530

    Working ACS using local resources

---
+++ ACSContosoDemo/AdsCommon/ContosoAdsContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace AdsCommon
+{
+    public class AdsContext : DbContext
+    {
+        public AdsContext() : base("name=ContosoAdsContext")
+        {
+        }
+
+        public AdsContext(string connString) : base(connString)
+        {
+        }
+
+        public System.Data.Entity.DbSet<Ad> Ads { get; set; }
+    
+    }
+}
